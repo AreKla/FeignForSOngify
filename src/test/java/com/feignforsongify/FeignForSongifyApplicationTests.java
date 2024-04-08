ae5fc1649a3ee3f6ccefe3a9c9842a8f6f391b98
klakd <arkadiusz.klak@gmail.com>
2024-04-08 12:29:12 +0200

    Initial Commit

---
+++ src/test/java/com/feignforsongify/FeignForSongifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feignforsongify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignForSongifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
